a1d7d0d23825da77e31c371adccf79314d423251
SalmanShaik2668 <salmans@thoughtworks.com>
2019-12-27 22:32:20 +0530

    Revert "Adding https"
    This reverts commit 52db16a9fd38393ca9e662132ba40c6cb693860e.

--- server.js
+++ server.js
@@ -1,15 +1,8 @@
-const https = require("https");
-const http = require("http");
-const fs = require("fs");
+const http = require('http');
 const app = require('./src/app.js');
 
-const privateKey = fs.readFileSync('privatekey.pem').toString();
-const certificate = fs.readFileSync('certificate.pem').toString();
-
-const options = {key: privateKey, cert: certificate};
 const PORT = process.env.PORT || 8000;
 
-http.createServer(app).listen(PORT)
-https.createServer(options, app).listen(443)
-console.log(`server is listening in http: ${PORT}.`);
-console.log(`server is listening in https: 443.`);
+let server = http.createServer(app);
+server.listen(PORT);
+console.log(`server is listening to ${PORT}.`);
